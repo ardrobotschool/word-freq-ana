47330d4d335ff73ed54d4111d36d0d6b019f53b8
Artur Drobot <drobotarthur@gmail.com>
2017-05-19 19:10:04 +0000

    Add word frequency reporting.

--- word-freq-ana.cpp
+++ word-freq-ana.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 int main(){
     vector<string*> words;
+    vector<string*> * wordsByLength[32];
     
     char filename[32];
     cout << "Enter filename: ";
     cin >> filename;
     ifstream is(filename);
     
+    //Reading file:
     if(is.is_open()){
         char ch;
         while(is){
@@ -27,11 +29,25 @@ int main(){
             }
         }
     }
+    //Calculating mean word length:
     int charcount = 0;
     for(int i = 0; i < words.size(); i++){
-        charcount += (*(words[i])).length();
+        charcount += (words[i])->length();
     }
     double meanLength = charcount;
     meanLength/=words.size();
     cout << "Mean word length: " << meanLength << endl;
+    
+    //Categorizing words by length:
+    for(int i = 0; i < 32; i++){
+        wordsByLength[i] = new vector<string*>;
+    }
+    for(int i = 0; i < words.size(); i++){
+        wordsByLength[words[i]->length()]->push_back(words[i]);
+    }
+    for(int i = 0; i < 32; i++){
+        if(!wordsByLength[i]->empty()){
+            cout << i << "-letter words: " << wordsByLength[i]->size() << endl;
+        }
+    }
 }
